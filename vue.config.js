d972e6630e0cb8cbfc817971f4e1a8a71c4a81be
cooloozhang <cooloozhang@foxmail.com>
2022-06-06 15:30:06 +0800

    sd

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@$', resolve('src'))
+  },
+
+  productionSourceMap: false,
+  // lintOnSave: process.env.NODE_ENV !== 'production',
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
+  outputDir: 'dist',
+  assetsDir: 'assets'
 })
